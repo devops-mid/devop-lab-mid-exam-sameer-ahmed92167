b87fd721a8392183297c3945b6f7616dbb3b5ab4
sameer-ahmed92167 <157287549+sameer-ahmed92167@users.noreply.github.com>
2025-03-19 08:20:37 +0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    reg = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(15))  # Optional field
